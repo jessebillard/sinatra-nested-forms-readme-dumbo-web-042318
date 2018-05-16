9bcfe8d71346b2fceb645b5d40eff9dd5112f787
jessebillard <jessebillard@mac.com>
2018-05-16 15:02:56 -0400

    sinatra nested forms readme

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :new
+    end 
+
+    post '/student' do
+        @student = Student.new(params["student"])
+
+        params["student"]["course"].each do |course_details_hash|
+            Course.new(course_details_hash)
+        end 
+        @courses = Course.all
+
+        erb :student
+    end 
+
 end
